ef6e6d446c5a946c6d571ca758e261236bca44b4
Rustie <64870518+woodcox@users.noreply.github.com>
2023-05-30 15:08:05 +0100

    Amend generateScss.mjs to generate scss modules (#6)

--- package/generateScss.mjs
+++ package/generateScss.mjs
@@ -1,6 +1,6 @@
 import Sizes from 'open-props/src/sizes';
 import Colors from 'open-props/src/colors';
-import ColorsHSL from 'open-props/src/colors-hsl';
+import ColorsHsl from 'open-props/src/colors-hsl';
 import { StaticShadows as Shadows } from 'open-props/src/shadows';
 import Aspects from 'open-props/src/aspects';
 import Borders from 'open-props/src/borders';
@@ -9,9 +9,9 @@ import Easings from 'open-props/src/easing';
 import Gradients from 'open-props/src/gradients';
 import Svg from 'open-props/src/svg';
 import Zindex from 'open-props/src/zindex';
-import MaskEdges from 'open-props/src/masks.edges';
-import MaskCornerCuts from 'open-props/src/masks.corner-cuts';
-import { CustomMedia } from 'open-props/src/media';
+import MasksEdges from 'open-props/src/masks.edges';
+import MasksCornerCuts from 'open-props/src/masks.corner-cuts';
+import { CustomMedia as Media } from 'open-props/src/media';
 // import Animations from 'open-props/src/animations';
 
 import fs from 'node:fs/promises';
@@ -21,44 +21,74 @@ import { fileURLToPath } from 'node:url';
 import { CustomMediaHelper } from './CustomMediaHelper.mjs';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const customMediaHelper = new CustomMediaHelper(CustomMedia);
+const customMediaHelper = new CustomMediaHelper(Media);
 
-let generatedScss = '@use "sass:list";\n';
+const openPropFiles = {
+  'media': Media,
+  'sizes': Sizes,
+  'colors': Colors,
+  'colors-hsl': ColorsHsl,
+  'shadows': Shadows,
+  'aspects': Aspects,
+  'borders': Borders,
+  'fonts': Fonts,
+  'easings': Easings,
+  'gradients': Gradients,
+  'svg': Svg,
+  'zindex': Zindex,
+  'masks.edges': MasksEdges,
+  'masks.corner-cuts': MasksCornerCuts,
+};
 
-Object.entries({
-	...Sizes,
-	...Colors,
-	...ColorsHSL,
-	...Shadows,
-	...Borders,
-	...Fonts,
-	...Easings,
-	...Gradients,
-	...Svg,
-	...Zindex,
-	...MaskEdges,
-	...MaskCornerCuts,
-}).forEach(([key, value]) => {
-	if (key.includes('@')) {
-		return;
-	}
-	key = key.replace('--', '$');
-	generatedScss += `${key}: ${value};\n`;
-});
+const writeSCSSModule = async (moduleName, content) => {
+  const outFile = path.join(__dirname, `${moduleName}.scss`);
+  await fs.writeFile(outFile, content, { encoding: 'utf-8' });
+};
 
-Object.entries(Aspects).forEach(([key, value]) => {
-	key = key.replace('--', '$');
-	if (value.includes('/')) {
-		value = `list.slash(${value.replace('/', ',')})`; // fix sass deprecation warning: https://sass-lang.com/documentation/breaking-changes/slash-div
-	}
-	generatedScss += `${key}: ${value};\n`;
-});
+const generateSCSSModule = async (moduleName, importObj) => {
+  let generatedScss = '';
+  
+  if (moduleName.toLowerCase() === 'aspects') {
+    generatedScss = '@use "sass:list";\n';
+    
+    Object.entries(importObj).forEach(([key, value]) => {
+      key = key.replace('--', '$');
+      if (value.includes('/')) {
+        value = `list.slash(${value.replace('/', ',')})`; // fix sass deprecation warning: https://sass-lang.com/documentation/breaking-changes/slash-div
+      }
+      generatedScss += `${key}: ${value};\n`;
+    });
+    
+  } else if (moduleName.toLowerCase() === 'media') {
+    Object.keys(importObj).forEach((queryName) => {
+      const processedQuery = customMediaHelper.process(queryName);
+      queryName = queryName.replace('--', '$');
+      generatedScss += `${queryName}: '${processedQuery}';\n`;
+    });
+    
+  } else {
+    Object.entries(importObj).forEach(([key, value]) => {
+      if (key.includes('@')) {
+        return;
+      }
+      key = key.replace('--', '$');
+      generatedScss += `${key}: ${value};\n`;
+    });
+  }
 
-Object.keys(CustomMedia).forEach((queryName) => {
-	const processedQuery = customMediaHelper.process(queryName);
-	queryName = queryName.replace('--', '$');
-	generatedScss += `${queryName}: '${processedQuery}';\n`;
-});
+  await writeSCSSModule(moduleName, generatedScss);
+};
 
-const outFile = path.join(__dirname, 'index.scss');
-await fs.writeFile(outFile, generatedScss, { encoding: 'utf-8' });
+
+for (const [moduleName, importObj] of Object.entries(openPropFiles)) {
+  generateSCSSModule(moduleName, importObj);
+}
+
+// Generate index.scss
+let indexScss = '';
+for (const moduleName in openPropFiles) {
+  indexScss += `@forward '${moduleName}';\n`;
+}
+
+const indexOutFile = path.join(__dirname, 'index.scss');
+await fs.writeFile(indexOutFile, indexScss, { encoding: 'utf-8' });
