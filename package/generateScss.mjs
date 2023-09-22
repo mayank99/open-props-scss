ad6b3a2aad04a2f038a3db9d5dd5783790087eda
Rustie <64870518+woodcox@users.noreply.github.com>
2023-09-22 18:49:36 +0100

    Generate animations.scss (#11)

--- package/generateScss.mjs
+++ package/generateScss.mjs
@@ -12,7 +12,7 @@ import Zindex from 'open-props/src/zindex';
 import MasksEdges from 'open-props/src/masks.edges';
 import MasksCornerCuts from 'open-props/src/masks.corner-cuts';
 import { CustomMedia as Media } from 'open-props/src/media';
-// import Animations from 'open-props/src/animations';
+import Animations from 'open-props/src/animations';
 import OklchColors from 'open-props/src/props.colors-oklch.js';
 import OklchHues from 'open-props/src/props.colors-oklch-hues.js';
 
@@ -31,6 +31,7 @@ const openPropFiles = {
 	colors: Colors,
 	'colors-hsl': ColorsHsl,
 	shadows: Shadows,
+	animations: Animations,
 	aspects: Aspects,
 	borders: Borders,
 	fonts: Fonts,
@@ -61,6 +62,68 @@ const generateSCSSModule = async (moduleName, importObj) => {
 			}
 			generatedScss += `${key}: ${value};\n`;
 		});
+
+	// animations.scss
+  	} else if (moduleName.toLowerCase() === 'animations') {
+  	generatedScss = "@use 'easings' as _e;\n@use 'media' as _mq;\n@use 'sass:string';\n$id: string.unique-id();\n\n";
+
+  	const createAnimationMixin = (animationName, keyframesContent, duration, easing) => {
+   		return `@mixin ${animationName} {
+			$name: op-#{$id}-${animationName}; ${keyframesContent}
+			animation: #{$name} ${duration} ${easing};
+		}\n`;
+  	};
+
+ 	const createDarkAnimationMixin = (darkName, darkKeyframesContent, darkDuration, darkEasing) => {
+		return `@mixin ${darkName} {
+			$name: op-#{$id}-${darkName}; ${darkKeyframesContent}
+			animation: #{$name} ${darkDuration} ${darkEasing};
+		}\n`;
+	};
+
+  	let animationsStr = '';
+
+  	Object.entries(importObj).forEach(([key, value]) => {
+    		if (value.includes('@keyframes')) {
+      			let animationName = key.replace('--animation-', '');
+
+      			// Check if the animation name ends with "-@"
+      			if (animationName.endsWith('-@')) {
+        			animationName = animationName.slice(0, -2);
+      			}
+
+      			// Remove "@media:" if it exists
+      			animationName = animationName.replace('@media:', '');
+      
+      			const keyframesContent = value.replace(/@keyframes\s+(\S+)/, '@keyframes #{$name}');
+
+      			const animationKey = `--animation-${animationName}`;
+      
+      			if (importObj[animationKey]) {
+        			const animationParts = importObj[animationKey].split(' ');
+        			const duration = animationParts[1];
+        			const easing = animationParts[2].replace(/var\(--(.*?)\)/g, '#{_e.$$$1}');
+        			animationsStr += createAnimationMixin(animationName, keyframesContent, duration, easing);
+      			}
+    		}
+		
+    		// Dark animations and keyframes
+    		if (key.includes('@media:dark')) {
+      			const keyframesParts = value.split(' ');
+      			const darkKeyframesContent = value.replace(/@keyframes\s+(\S+)/, '@keyframes #{$name}');
+      			const lightName = keyframesParts[1];
+      			const d = '-dark';
+      			const a = '--animation-'
+      			const darkName = lightName + d;
+      			const lightAnimation = a + lightName;
+      			const animationPart = Animations[lightAnimation].split(' ');
+      			const darkDuration = animationPart[1];
+      			const darkEasing = animationPart[2].replace(/var\(--(.*?)\)/g, '#{_e.$$$1}');
+      			animationsStr += createDarkAnimationMixin(darkName, darkKeyframesContent, darkDuration, darkEasing);
+    		}
+ 	});
+
+  	generatedScss += `${animationsStr}`;
 
 		// media.scss
 	} else if (moduleName.toLowerCase() === 'media') {
